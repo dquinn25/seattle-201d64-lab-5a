7fcfe8eeb60611bf54d6dbfb318ad6e9925aab12
David Quinn <david.c.quinn25@gmail.com>
2020-06-05 20:51:04 -0700

    problem 3

--- starter-code/app.js
+++ starter-code/app.js
@@ -30,10 +30,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-  console.log('The product of ' + a + ' and ' + b + ' is ' + (a + b) +'.');
-  return [(a * b), 'The product of ' + a + ' and ' + b + ' is ' + (a * b) +'.'];
-}
 
+  return [(a * b),  'The product of ' + a + ' and ' + b + ' is ' + (a * b) +'.']
+}
 // Here is the test for multiply(); uncomment it to run it
 testMultiply(5,9);
 
@@ -41,22 +40,35 @@ testMultiply(5,9);
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+  Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
 
-Third element: "4 and 7 and 5 sum to 16."
-Fourth element: "The product of 4 and 7 and 5 is 140."
+  Third element: "4 and 7 and 5 sum to 16."
+  Fourth element: "The product of 4 and 7 and 5 is 140."
 
-IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+  IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
+  Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
+  function sumAndMultiply(a, b, c) { //eslint-disable-line
+  console.log('The product of ' + a + ' and ' + b + ' is ' + (a + b) +'.');
+  var answer1 = sum (a, b)
+  var answer2 = sum (answer1[0], c) 
+  var finalSum = answer2[0]
+  var sumSentence = a + ' and ' +  b + ' and ' + c + ' sum to ' + finalSum +  '.';
 
+  var mulAnswer1 = multiply (a, b)
+  var mulAnswer2 = multiply (mulAnswer1[0], c)
+  var finalMul = mulAnswer2[0]
+  var multiplySentence = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + finalMul + '.'
+
+  var finalOutput = [finalSum, finalMul,  sumSentence, multiplySentence]
+  return finalOutput
 }
 
+
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -101,7 +113,7 @@ function multiplyArray(multArr) { //eslint-disable-line
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyArray(testArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
 
